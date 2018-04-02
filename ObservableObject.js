7b690e23c8d0ab2e15923ef7d6d5706b195e0659
ICO <DnaBoss@users.noreply.github.com>
2018-04-02 09:20:35 +0800

    Update ObservableObject.js

--- ObservableObject.js
+++ ObservableObject.js
@@ -1,35 +1,35 @@
 function watch(object, onChange) {
-        const handler = {
-            set(target, key, value, receiver) {
-                if (target[key] !== value) {
-                    onChange(target, key, value);
-                }
-                return Reflect.set(target, key, value, receiver);
-            },
-            deleteProperty(target, property) {
-                onChange(property);
-                return Reflect.deleteProperty(target, property);
+    const handler = {
+        set(target, key, value, receiver) {
+            if (target[key] !== value) {
+                onChange(target, key, value);
             }
-        };
-
-        return new Proxy(object, handler);
+            return Reflect.set(target, key, value, receiver);
+        },
+        deleteProperty(target, property) {
+            onChange(property);
+            return Reflect.deleteProperty(target, property);
+        }
     };
 
-    let test = {
-        gg: 1
-    };
+    return new Proxy(object, handler);
+};
 
-    let changeable = watch(test, (obj, property, value) => {
-        console.log(`test 的 ${property} 由 ${test[property]} 改變為 ${value}`)
-    });
+let test = {
+    gg: 1
+};
 
-    //再設定一樣的值，但不會觸發通知
-    changeable.gg = 1;
-    //這次改成不一樣的值，就會觸發通知道
-    changeable.gg = 2;
-    //再設定一樣的值，但不會觸發通知
-    changeable.gg = 2;
-    //這次改成不一樣的值，就會觸發通知道
-    changeable.gg = 3;
-    //新增一個屬性也會收到通知
-    changeable.b = 5;
+let changeable = watch(test, (obj, property, value) => {
+    console.log(`test 的 ${property} 由 ${test[property]} 改變為 ${value}`)
+});
+
+//再設定一樣的值，但不會觸發通知
+changeable.gg = 1;
+//這次改成不一樣的值，就會觸發通知道
+changeable.gg = 2;
+//再設定一樣的值，但不會觸發通知
+changeable.gg = 2;
+//這次改成不一樣的值，就會觸發通知道
+changeable.gg = 3;
+//新增一個屬性也會收到通知
+changeable.b = 5;
