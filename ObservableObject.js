0bc40d4f2544681adbeb109b1835b6ffbad574bf
ICO <DnaBoss@users.noreply.github.com>
2018-04-02 09:18:23 +0800

    Update ObservableObject.js

--- ObservableObject.js
+++ ObservableObject.js
@@ -1,30 +1,34 @@
 function watch(object, onChange) {
-    const handler = {
-        set(target, key, value, receiver) {
-            if (target[key] !== value) {
-                onChange(target, key, value);
-            }  
-            return Reflect.set(target, key, value, receiver);
-        },
-        deleteProperty(target, property) {
-            onChange(property);
-            return Reflect.deleteProperty(target, property);
-        }
+        const handler = {
+            set(target, key, value, receiver) {
+                if (target[key] !== value) {
+                    onChange(target, key, value);
+                }
+                return Reflect.set(target, key, value, receiver);
+            },
+            deleteProperty(target, property) {
+                onChange(property);
+                return Reflect.deleteProperty(target, property);
+            }
+        };
+
+        return new Proxy(object, handler);
     };
 
-    return new Proxy(object, handler);
-};
+    let test = {
+        gg: 1
+    };
 
-let j = {
-    a: 1
-};
+    let changeable = watch(test, (obj, property, value) => {
+        console.log(`test 的 ${property} 由 ${test[property]} 改變為 ${value}`)
+    });
 
-let changeable = watch(j, (obj, property, value) => {
-   
-});
-
-changeable.a = 1;
-
-changeable.a = 2;
-
-
+    //再設定一樣的值，但不會觸發通知
+    changeable.gg = 1;
+    //這次改成不一樣的值，就會觸發通知道
+    changeable.gg = 2;
+    //再設定一樣的值，但不會觸發通知
+    changeable.gg = 2;
+    //這次改成不一樣的值，就會觸發通知道
+    changeable.gg = 3;
+    changeable.b = 5;
