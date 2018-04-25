3c8c07a7f027391aa89360dd34c7ed9a599263ba
cash <cash@cashde-MacBook-Pro.local>
2018-04-25 08:25:14 +0800

    [ADD]將工廠改用lut

--- Factory/SimpleFactory/SimpleFactory.js
+++ Factory/SimpleFactory/SimpleFactory.js
@@ -1,25 +1,20 @@
-
-abstract class Car {
-    abstract installEngine(): void;
-    abstract installWhell(): void;
-    abstract run(): void;
+class Car {
 }
 class Porsche911 extends Car {
-    installEngine() {/*想怎麼實作就怎麼實作，你家的事 */ }
-    installWhell() {/*想怎麼實作就怎麼實作，你家的事 */ }
-    run() { console.log('Porsche911 帥氣的奔馳') }
+    installEngine() { }
+    installWhell() { }
+    run() { console.log('Porsche911 帥氣的奔馳'); }
 }
 class Boxter extends Car {
-    installEngine() {/*想怎麼實作就怎麼實作，你家的事 */ }
-    installWhell() {/*想怎麼實作就怎麼實作，你家的事 */ }
-    run() { console.log('Boxter 帥氣的奔馳') }
+    installEngine() { }
+    installWhell() { }
+    run() { console.log('Boxter 帥氣的奔馳'); }
 }
 class Cayenne extends Car {
-    installEngine() {/*想怎麼實作就怎麼實作，你家的事 */ }
-    installWhell() {/*想怎麼實作就怎麼實作，你家的事 */ }
-    run() { console.log('Cayenne 帥氣的奔馳') }
+    installEngine() { }
+    installWhell() { }
+    run() { console.log('Cayenne 帥氣的奔馳'); }
 }
-
 // class CarStore {
 //     car: Car;
 //     orderCar(type: string) {
@@ -37,7 +32,6 @@ class Cayenne extends Car {
 //         return this.car;
 //     }
 // }
-
 // class SimpleCarFactory {
 //     car: Car;
 //     createCar(type: string) {
@@ -53,46 +47,40 @@ class Cayenne extends Car {
 //         return this.car;
 //     }
 // }
-
 class SimpleCarFactory {
-    car: Car;
-    createCar(type: string): Car {
-        let model: { [Type: string]: Car } = {
+    createCar(type) {
+        let model = {
             '911': new Porsche911(),
             'boxter': new Boxter(),
             'cayenne': new Cayenne(),
-        }
-        return model[type]
+        };
+        return model[type];
     }
 }
-
 class CarStore {
-    car: Car;
-    factory: SimpleCarFactory;
-    constructor(factory: SimpleCarFactory) {
+    constructor(factory) {
         this.factory = factory;
     }
-    orderCar(type: string) {
+    orderCar(type) {
         this.car = this.factory.createCar(type);
         this.car.installEngine();
         this.car.installWhell();
         return this.car;
     }
 }
-
-/**  
+/**
  * SimpleCarFactory 負責 車子的生造、建造
  * 更精確的說 負責「實例化」 讓工廠去負責 new 一個類別，產生實例
  * 而 CarStore 負則使用
  * 要什麼樣子的車，是外部輸入的，真正的車子是工廠給的
  * 門市 接受訂單 交給工廠 不管交回來的車子是什麼型號，都只執行一樣的步驟、方法
- * 有新的車型出來，再去 工廠新增一個判斷 
+ * 有新的車型出來，再去 工廠新增一個判斷
  * CarStore 透過 SimpleCarFactory 取得 具體的 Car
  * 這就是簡單工廠，沒什麼好說的，因為它就是「簡單」
  * 但卻實實在在的經常被使用
 */
-
-let factory: SimpleCarFactory = new SimpleCarFactory();
-let store: CarStore = new CarStore(factory);
-let car: Car = store.orderCar("911");
-car.run(); //Porsche911 帥氣的奔馳
+let factory = new SimpleCarFactory();
+let store = new CarStore(factory);
+let car = store.orderCar("911");
+car.run();
+//# sourceMappingURL=SimpleFactory.js.map
